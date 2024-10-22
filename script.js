a2298c09a3178dee800bab3ff1d059830ed3f665
unknown <2300016080@webmail.uad.ac.id>
2024-10-22 14:55:25 +0700

    Attempt 1 -16.20

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-function generatePassword(length, options) {
-    
+export function generatePassword(length, options) {
+
     // TODO: Create a variable for the character set based on selected options
     let characters = '';
-    if (options.includeUppercase){
+    if (options.includeUppercase) {
         characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
-    if (options.includeLowercase){
+    if (options.includeLowercase) {
         characters += 'abcdefghijklmnopqrstuvwxyz';
     }
-    if (options.includeNumbers){ 
+    if (options.includeNumbers) {
         characters += '0123456789';
     }
-    if (options.includeSpecialChars){
+    if (options.includeSpecialChars) {
         characters += '!@#$%^&*()';
     }
 
 
-// TODO: Generate the password based on the selected criteria
+    // TODO: Generate the password based on the selected criteria
     let password = '';
     for (let i = 0; i < length; i++) {
         password += characters.charAt(Math.floor(Math.random() * characters.length));
     }
-// TODO: Add event listener to the button to call generatePassword and display the output
+    // TODO: Add event listener to the button to call generatePassword and display the output
     return password;
 }
 // BONUS: Implement the copy to clipboard functionality
